0194d2c6dc787b9f0bfb6a00a89531b00e2e07bc
ishameed <ishameed@umich.edu>
2024-04-08 20:12:05 -0400

    fix app

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
 from flask import Flask, request, jsonify
 from joblib import load
 import pandas as pd
+import os
+import boto3
+from werkzeug.exceptions import InternalServerError
 
 app = Flask(__name__)
-model = load('random_forest_member_type_model.joblib')
+# model = load('random_forest_member_type_model_final.joblib')
+
+# # AWS S3 Configuration
+# BUCKET_NAME = 'heroku-model'  # replace with your bucket name
+# MODEL_FILE_NAME = 'random_forest_member_type_model_final.joblib'  # replace with your model file name
+# MODEL_PATH = '/tmp/' + MODEL_FILE_NAME  # '/tmp' is writable on Heroku
+
+MODEL_URL = 'https://heroku-model.s3.amazonaws.com/random_forest_member_type_model.joblib' 
+MODEL_PATH = 'random_forest_member_type_model_final.joblib'
+
+@app.before_first_request
+def download_model():
+    # Check if the model is already present locally
+    if not os.path.isfile(MODEL_PATH):
+        print("Downloading model from S3...")
+        response = requests.get(MODEL_URL, stream=True)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            with open(MODEL_PATH, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        else:
+            print(f"Failed to download model, status code: {response.status_code}")
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -14,4 +40,5 @@ def predict():
     return jsonify(predictions.tolist())
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
